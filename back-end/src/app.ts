5c037ab2fd93573a66b5f5fcba232927fce221fa
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-09-26 01:06:57 -0300

    implementação do teste e2e de upvote e downvote no front

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,11 +2,19 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+import e2eRouter from "./routers/e2eRouter.js";
 import recommendationRouter from "./routers/recommendationRouter.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'test') {
+    app.use("/e2e", e2eRouter)
+}
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
--- front-end/cypress/e2e/homeRote.cy.js
+++ front-end/cypress/e2e/homeRote.cy.js
@@ -1,5 +1,9 @@
 import { faker } from "@faker-js/faker";
 
+beforeEach(async () => {
+  await cy.request('POST', 'http://localhost:5000/e2e/reset', {});
+});
+
 describe('Testa interações com as recomendações', () => {
   it('Deve criar uma nova recomendação', () => {
     cy.visit('http://localhost:3000');
@@ -14,5 +18,8 @@ describe('Testa interações com as recomendações', () => {
 
     cy.wait('@insertRecommendation');
     cy.wait('@getRecommendations');
+
+    cy.get('#upvote').click();
+    cy.get('#downvote').click();
   })
 })
